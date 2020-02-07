a99291bd16ef5a14d0ace10cfbfa262cd52081fd
prabhurao39 <52779482+prabhurao39@users.noreply.github.com>
2020-02-07 19:11:30 +0530

    Add files via upload

--- MarchDis/src/main/java/com/pkg/Application.java
+++ MarchDis/src/main/java/com/pkg/Application.java
@@ -1,24 +1,21 @@
 package com.basic.log4j;
 
-// import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
 
 import org.apache.log4j.PropertyConfigurator;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 
 public class Application {
-  // private final static Logger logger = Logger.getLogger(Application.class);
-  private static final Logger LOGGERS = LoggerFactory.getLogger(Application.class);
-
-  public static void main(String[] args) {
-
-    // PropertiesConfigurator is used to configure logger from properties file
-    PropertyConfigurator.configure(
-        "C:\\Users\\prabhurao\\eclipse-workspace\\MarchDis\\src\\main\\resources\\log4j.properties");
-
-    LOGGERS.info("main start ............ ");
-    System.out.println("Hello Siddharth");
-    LOGGERS.debug("main end ............  ");
-  }
+    final static Logger logger = Logger.getLogger(Application.class);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    
+    public static void main(String[] args) {
+      
+    //PropertiesConfigurator is used to configure logger from properties file
+      PropertyConfigurator.configure("C:\\Users\\prabhurao\\eclipse-workspace\\MarchDis\\src\\main\\resources\\log4j.properties");
+      
+        logger.info("main start ............ "); 
+        System.out.println("Hello Siddharth");
+        logger.debug("main end ............  ");
+    }
 }
 
