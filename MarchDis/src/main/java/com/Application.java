7a3550329157d27670aae609fe4afa2f86fc5e91
prabhurao39 <52779482+prabhurao39@users.noreply.github.com>
2020-02-08 21:47:37 +0530

    Add files via upload

---
+++ MarchDis/src/main/java/com/Application.java
@@ -0,0 +1,21 @@
+package com.basic.log4j;
+
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+
+public class Application {
+    final static Logger logger = Logger.getLogger(Application.class);
+//    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+    
+    public static void main(String[] args) {
+      
+    //PropertiesConfigurator is used to configure logger from properties file
+      PropertyConfigurator.configure("C:\\Users\\prabhurao\\eclipse-workspace\\MarchDis\\src\\main\\resources\\log4j.properties");
+      
+        logger.info("main start ............ "); 
+        System.out.println("Hello Siddharth");
+        logger.debug("main end ............  ");
+    }
+}
+
